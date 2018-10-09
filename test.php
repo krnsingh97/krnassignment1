a0095aa08392c14a79800e88bf43b9fe94cc8982
karan singh <41100240+krnsingh97@users.noreply.github.com>
2018-10-09 16:45:36 -0400

    Add files via upload

---
+++ test.php
@@ -0,0 +1,189 @@
+<?php
+header('Content-Type: text/html; charset=iso-8859-1');
+?>
+<!DOCTYPE html>
+<html lang="sv">
+  <head>
+    <meta charset="ISO-8859-1">
+    <title>Canada</title>
+    <meta name="description" content="JTB - automated reporting platform control panel">
+    <meta name="author" content="Jonathan Wening (eu8300) EVRY Sverige">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-switch.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <script type="text/javascript" src="assets/js/jquery-2.2.2.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap-switch.min.js"></script>
+	<body bgcolor="#E6E6FA">
+    <style type="text/css">
+      body {
+         background-image: url(wall.jpg);
+          padding-top: 0px;
+          padding-bottom: 40px;
+      }
+      
+      .border-rounded {
+          -webkit-border-radius: 7px;
+          -moz-border-radius: 7px;
+          border-radius: 7px;
+          padding: 10px;
+      }
+      
+      .header {
+          border-bottom: 1px solid #CCC;
+      }
+      
+      .menu {
+          padding-top: 10px;
+          border-right: 1px solid #CCC;
+          min-height: 300px;
+          max-width: 200px;
+      }
+      
+      .content {
+          padding-top: 10px;
+          padding-left: 50px;
+      }
+      
+      .icon-red {
+          color: red;
+      }
+      
+      .icon-green {
+          color: green;
+      }
+	body {
+    background-color: yellow;
+}
+        div.krn
+        {
+         
+           height: 100px;
+            width: 250px;
+           
+        }
+        .krn h1
+        {
+            font-family: sans-serif;
+            color: yellow;
+        }
+        .singh 
+        {
+            margin-left: 860px;
+            height: 500px;
+            width: 600px;
+            margin-bottom: 200px;
+        }
+        .singh h1
+        {
+            font-family: sans-serif;
+            color: yellow;
+        }
+        .atwal
+        {
+            margin-bottom: 100px;
+            margin-left: 100px;
+            margin-top: -230px;
+        }
+        .atwal h1
+        {
+            font-family: sans-serif;
+            color: yellow;
+            margin-left: 300px;
+        }
+        .para
+        {
+           
+            height: 400px;
+            width: 650px;
+            text-align: center;
+            margin-left: 450px;
+        }
+       .para h1
+        {
+            color: yellow;
+            font-size: 40px;
+            font-family: sans-serif;
+        }
+        
+    </style>
+	<style>
+div.a {
+    text-align: center;
+}
+div.b {
+    text-align: center;
+}
+</style>
+  </head>
+  <body>
+      <div class="container-fluid">
+          <div class="link">
+         
+          </div>
+          <div class="row header">
+              <div class="a"><h1><a href="#">Welcome to Rock pizza Canada! Hope you are having a good day!</a></h1></div>
+			 <div class="a"> <img src='krn.png'></div>
+          </div> <!-- /row end of header -->
+          <div class="row">
+              <div class="col-sm-2 col-md-2 menu">
+                  <div class="col-sm-10 col-md-10 content">
+                
+              </div>
+               
+                  <ul class="nav nav-pills nav-stacked">
+                      
+                      <li><a href="#"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span>Vagitarian</a></li>
+					  <br>
+                      <li><a href="#"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Neapolitan</a></li>
+					   <br>
+                      <li><a href="#"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Chicago Deep Dish</a></li>
+					   <br>
+                      <li><a href="#"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Chicago Thin Crust</a></li>
+					   <br>
+                      <li><a href="#"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Detroit Style</a></li>
+					   <br>
+                      <li><a href="#"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>New England Greek Style</a></li>
+					   <br>
+                      <li><a href="#"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>New York Thin Crust</a></li>
+					   <br>
+                      <li><a href="#"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>St. Louis Style</a></li>
+					   <br>
+                      <li><a href="#"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Tomato Pie: New Jersey style</a></li>
+                  </ul>
+              </div>
+              <div class="para" >
+                  <h1>Can you imagine a pizza so good that people who have eaten it have enjoyed it so much that they cannot stand to be without it?
+                   When the craving hits, and believe that it will, you will go to great lengths to be able to enjoy a Fricano's pizza just one more time.</h1>
+              
+              </div>
+              
+              <div class="krn" >
+                  <h1>pizza-neapolitan</h1>
+              
+                  <img src="pizza-neapolitan-56a310505f9b58b7d0d0382c.jpg" style="height:500px;" >
+        
+      </div>
+              
+          </div> 
+         
+      </div> 
+      <div class="singh" >
+                  <h1 >Vagitarian</h1>
+                  <img src="veg.jpg" style="" >
+              
+              </div>
+      <div class="atwal" >
+          <h1>Chicago Deep Dish</h1>
+          <img src="k.jpeg">
+      
+      </div>
+      
+      
+</body>
+    </head>
+    <footer>
+    <center><h1 style="color:purple" >since 2016</h1></center>
+    </footer>
+</html>
